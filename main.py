80c2ed9db74b644658f6bb8df8fff7cde4a03be7
Barashov <barashovmisha@gmail.com>
2023-08-02 21:23:00 +0300

    правки

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from multiprocessing.pool import Pool
 from settings import load_settings
 from utils import transcribe_file
 from settings import Settings
+import os
 
 
 def main():
     settings: Settings = load_settings()
-
+    os.makedirs(name='file', exist_ok=True)
     if settings.FILENAME:
         transcribe_file(settings.FILENAME)
     else:
